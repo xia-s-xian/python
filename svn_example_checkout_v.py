040c62961e20c1f46309333c07fee4949290c982
xiashengxian <903089151@qq.com>
2018-12-20 11:20:15 +0800

    add the function of control SVN version

--- svn_example_checkout_v.py
+++ svn_example_checkout_v.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+#pysvn 参考Url：http://pysvn.tigris.org/docs/pysvn_prog_ref.html#pysvn_revision
+
 import pysvn
 import time
 
 client = pysvn.Client()
 
-
 #check out the current version of the pysvn project
 
 path='C:/Users/DELL/Desktop/VA9638B_V8000_MultiCore'
-
 
 url_top_bt='http://10.0.2.88/svn/VimcBT/VA9638B/Src_Mod38/trunk/VA9638B_V8000_MultiCore'
 url_dsp='http://10.0.2.88/svn/VimcBT/VA9638B/Src_DSP/BTAudio_Digitalgain'
@@ -28,38 +28,54 @@ path_dsp_tools='D:/4_ZGW/VimcBT/Hawkeye/trunk/Release/MPTool/DSPTool'
 path_demoEQ='D:/4_ZGW/VimcBT/Hawkeye/trunk/Release/MPTool/DemoEQ'
 path_eck='D:/4_ZGW/VimcBT/VA9638B/ECK/Tri-Mode/'
 
+##默认情况下，版本设置为1，则pysvn将checkout svn 最新得版本，如果指定版本，checkou指定版本
+rev_top_bt = 1
+rev_dsp = 1
+rev_mpflash3_0 = 1
+rev_bluetunes = 1
+rev_dsp_tools = 1 
+rev_demoEQ = 1
+rev_eck = 1
+
 def svn_updata():
     rev=pysvn.Revision(pysvn.opt_revision_kind.number,3832)
     client.update(path,revision=rev)
 
-def svn_checkout(url,path):
-    client.checkout(url,
-    path,revision=pysvn.Revision(pysvn.opt_revision_kind.head))
+def svn_checkout(url,path,rev=1):
 
+    rev_num=pysvn.Revision( pysvn.opt_revision_kind.number, rev )
+    rev_head=pysvn.Revision(pysvn.opt_revision_kind.head)
+
+    if(rev!=1)
+        rev=rev_num
+    else:
+        rev=rev_head
+    
+    client.checkout(url,path,revision=rev)
 
 def svn_checkout_mod():
-    svn_checkout(url_top_bt,path_top_bt)
-    get_info(url_top_bt,3861)
+    svn_checkout(url_top_bt,path_top_bt，rev_top_bt)
+  #  get_info(url_top_bt,3861)
     print("checkout multicor end\n")
     
-    svn_checkout(url_dsp,path_dsp)
-    get_info(url_dsp,3778)
+    svn_checkout(url_dsp,path_dsp，rev_dsp)
+  #  get_info(url_dsp,3778)
     print("checkout dsp end\n")
     
-    svn_checkout(url_mpflash3_0,path_mpflash3_0)
-    get_info(url_mpflash3_0,3818)
+    svn_checkout(url_mpflash3_0,path_mpflash3_0，rev_mpflash3_0)
+ #   get_info(url_mpflash3_0,3818)
     print("checkout url_mpflash3_0 end\n")
     
-    svn_checkout(url_bluetunes,path_bluetunes)
-    get_info(url_bluetunes,3849)
+    svn_checkout(url_bluetunes,path_bluetunes，rev_bluetunes)
+  #  get_info(url_bluetunes,3849)
     print("checkout url_bluetunes end\n")
     
-    svn_checkout(url_dsp_tools,path_dsp_tools)
-    get_info(url_dsp_tools,3361)
+    svn_checkout(url_dsp_tools,path_dsp_tools，rev_dsp_tools)
+ #   get_info(url_dsp_tools,3361)
     print("checkout url_dsp_tools end\n")
     
-    svn_checkout(url_demoEQ,path_demoEQ)
-    get_info(url_demoEQ,2986)
+    svn_checkout(url_demoEQ,path_demoEQ，rev_demoEQ)
+ #   get_info(url_demoEQ,2986)
     print("checkout url_demoEQ end\n")
       
 
