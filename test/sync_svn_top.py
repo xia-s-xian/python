bb10908e659fa4e7c256f98810eeab3e56729c58
xiashengxian <xia.shengxian@zgmicro.com>
2019-04-22 16:57:14 +0800

    add

---
+++ test/sync_svn_top.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os
+import shutil
+
+def del_copy_top():
+    path="../../"
+    path2=os.path.abspath(path)
+    print(path2)
+
+if __name__=="__main__":
+    del_copy_top()
